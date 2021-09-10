f27fd64884587a498180a2db907e0cd850d6977c
Dan Zinger <Dan.Zinger@softwire.com>
2021-09-10 13:31:22 +0100

    add previous and next buttons to users page

--- public/src/myface/src/components/userspage.jsx
+++ public/src/myface/src/components/userspage.jsx
@@ -1,18 +1,33 @@
 import React, {useEffect, useState} from "react";
 
+
 export function UsersPage() {
     const [users, setContent] = useState();
+    const [pageUrl, setPageUrl] = useState("/users")
+    const [listStart, setListStart] = useState(1)
 
     useEffect(function() {
         async function fetchData() {
-            const response = await fetch(`http://localhost:3001/users`);
+            const response = await fetch(`http://localhost:3001`+pageUrl);
             const json = await response.json();
 
             setContent(json);
         }
 
         fetchData();
-    }, );
+    }, [pageUrl]);
+
+    function changePage (dir) {
+        if (dir === "next") {
+            
+            setListStart(listStart + 10)
+            setPageUrl(users.next)
+        } else {
+            
+            setListStart(listStart - 10)
+            setPageUrl(users.previous)
+        }
+    }
 
     if (!users){
         return <p>Content is loading...</p>
@@ -22,7 +37,7 @@ export function UsersPage() {
             <>
             <h2> User Profiles</h2>
             <section>
-                <ol>
+                <ol start = {listStart} >
                     {users.results.map( (user) => 
                         <a href = {"http://localhost:3000/users/"+ user.id}>
                         <li> 
@@ -35,11 +50,17 @@ export function UsersPage() {
                         </li>
                         </a>
                     )}
-                
-
                 </ol>
             </section>
+            <p>http://localhost:3001/{users.next}</p>
+            <p>{users.next}</p>
+            <footer> 
+                {(users.previous) ?  <button type="submit" onClick={() => changePage("previous")}> Previous</button> : <></>} 
+                {(users.next) ?  <button type="submit" onClick={() => changePage("next")}> Next</button> : <></>} 
+
+            </footer>
             </>
         )
     }
 }
+
